8a94af4669c71006337bf7c4b8024b13c39da2b1
carolniuyan <yanxinyi@grinnell.edu>
2020-02-26 17:42:25 -0600

    HW6 Automated Tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,15 +4,21 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_path
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Ruby on Rails Tutorial Sample App" 
+    # Test dynamic content load status
+    assert_not_nil :random_no
+    assert_not_nil "image_tag", "failed to load image"
+    assert_not_nil "p", "failed to load random sentence"
+
   end
+#  /redirects/ 
 
   test "should get help" do
     get help_path
     assert_response :success
     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
-  
+
   test "should get about" do
     get about_path
     assert_response :success
