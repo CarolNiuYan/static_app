0b2babba9d873fc6bb7e3cff24960a9e8f4f4978
carolniuyan <yanxinyi@grinnell.edu>
2020-03-23 16:16:37 -0500

    heroku debugging

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,43 +1,25 @@
 class UsersController < ApplicationController
 
-  def show
-    @user = User.find(params[:id])
-  end
+def show
+  @user = User.find(params[:id])
+end
 
-  def new
-    @user = User.new
+def new
+  @user = User.new
+end
+
+def create
+  @user = User.new(user_params)
+  if @user.save
+    flash[:success] = "Welcome to Language Lab!"
+    redirect_to @user
+  else 
+    render 'new'
   end
-  
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      flash[:success] = "Welcome to Language Lab!"
-      redirect_to @user
-    else 
-      render 'new'
-    end
-  end
-<<<<<<< HEAD
-  
-  def logged_in_user
-    unless logged_in?
-      flash[:danger] = "Please log in."
-      redirects_to login_url
-    end
-  end
-=======
+end
 
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
-
-#  def logged_in_user
-#    unless logged_in?
-#      flash[:danger] = "Please log in."
-#      redirects_to login_url
-#    end
-#  end
->>>>>>> sign-up
-
 end
